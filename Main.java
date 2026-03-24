7711e421d4c27f658fcda6f1c2b235219b5926a7
Muhammad Hamza Tahir <147407247+Hamza912141@users.noreply.github.com>
2026-03-24 09:18:39 +0000

    Update Main.java
    Matrix Multiplication

--- Main.java
+++ Main.java
@@ -10,3 +10,21 @@ public class Main {
     // 1. Standard Deviation
         double sd = StandardDeviation.calculate(data);
         System.out.println("Standard Deviation: " + sd);
+   // 2. Matrix Multiplication
+        int size = (int) Math.sqrt(data.size() / 2);
+        int[][] A = new int[size][size];
+        int[][] B = new int[size][size];
+
+        int index = 0;
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                A[i][j] = data.get(index++);
+
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                B[i][j] = data.get(index++);
+
+        int[][] result = MatrixMultiplication.multiply(A, B);
+        System.out.println("Matrix Multiplication Done");
+        
+        MatrixMultiplication.printMatrix(result);
